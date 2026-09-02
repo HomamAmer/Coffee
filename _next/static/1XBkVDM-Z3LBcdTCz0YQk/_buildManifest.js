f6f76f93f75f0a88c13a1431633d5f8f704f1019
HomamAmer <homama964@email.com>
2026-09-02 08:33:28 +0300

    Updates

--- _next/static/1XBkVDM-Z3LBcdTCz0YQk/_buildManifest.js
+++ _next/static/1XBkVDM-Z3LBcdTCz0YQk/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Coffee//_next/:path+",
+        "destination": "/Coffee/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
